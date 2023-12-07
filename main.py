66b6d642d1a528eba84f8f2f2717361c867a5e3f
Zzar23 <maksimchibotar@gmail.com>
2023-12-07 20:36:49 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,42 +1,59 @@
-import datetime
-import aiogram
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+import telebot
+from telebot import types
+from datetime import datetime
 
+bot = telebot.TeleBot("6930938494:AAE_wIPMvHkxrdeDRcSr5NLb_RpKLv0Ytgw")
 
-TOKEN = 'TOKEN'
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+@bot.message_handler(commands=["start", "help"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("Добавить заметку"))
+    markup.add(types.KeyboardButton("Мои заметки"))
 
+    if message.text.lower() == "/help":
+        bot.send_message(message.chat.id, "Доступные команды:\n/start - начать работу с ботом\n/help - список команд")
+    else:
+        bot.send_message(message.chat.id, "Выберите действие", reply_markup=markup)
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.reply("Привет! Для записи заметки введите /note <текст заметки>")
+@bot.message_handler(func=lambda message: message.text.lower() == "добавить заметку")
+def add_note_start(message):
+    msg = bot.send_message(message.chat.id, "Введите вашу заметку:")
+    bot.register_next_step_handler(msg, add_note_process)
 
+def add_note_process(message):
+    user_id = message.chat.id
+    note = message.text
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-@dp.message_handler(commands=['note'])
-async def note(message: types.Message):
     try:
-        note_text = message.text.split('/note ', maxsplit=1)[1]
-        date = datetime.datetime.now().strftime("%Y-%m-%d")
-        with open(f"notes_{date}.txt", 'a') as file:
-            file.write(f"{date}: {note_text}\n")
-        await message.reply("Заметка сохранена!")
-    except IndexError:
-        await message.reply("Пожалуйста, введите текст заметки после команды /note")
+        with open(f"{user_id}_diary.txt", "a", encoding="utf-8") as file:
+            file.write(f"{date}: {note}\n")
+        bot.send_message(user_id, "Заметка успешно добавлена!")
 
+        # Возвращаем пользователя к начальной клавиатуре
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton("Добавить заметку"))
+        markup.add(types.KeyboardButton("Мои заметки"))
+        bot.send_message(user_id, "Выберите действие", reply_markup=markup)
 
-@dp.message_handler(commands=['help'])
-async def help(message: types.Message):
-    help_text = """
-    Доступные команды:
-    /start - начать работу с ботом
-    /help - список команд
-    /note <текст заметки> - добавить заметку
-    """
-    await message.reply(help_text)
+    except Exception as e:
+        bot.send_message(user_id, f"Произошла ошибка: {str(e)}")
 
+@bot.message_handler(func=lambda message: message.text.lower() == "мои заметки")
+def view_notes(message):
+    user_id = message.chat.id
 
-if __name__ == '__main__':
-    executor.start_polling(dp)
+    try:
+        with open(f"{user_id}_diary.txt", "r", encoding="utf-8") as file:
+            notes = file.read()
+
+        if notes:
+            bot.send_message(user_id, f"Ваши заметки:\n{notes}")
+        else:
+            bot.send_message(user_id, "У вас пока нет заметок.")
+
+    except Exception as e:
+        bot.send_message(user_id, f"Произошла ошибка: {str(e)}")
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
